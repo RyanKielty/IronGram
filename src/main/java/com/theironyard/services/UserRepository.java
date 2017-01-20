77c1335cc7644422bcca951e71a2fba5ba05f4db
ryankielty <rpkielty@gmail.com>
2017-01-20 10:43:43 -0600

    Initial commit - no requirements have been completed

---
+++ src/main/java/com/theironyard/services/UserRepository.java
@@ -0,0 +1,12 @@
+package com.theironyard.services;
+
+import com.theironyard.entities.User;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by ryankielty on 1/3/17.
+ */
+public interface UserRepository extends CrudRepository <User, Integer> {
+    User findFirstByName(String name);
+}
+
